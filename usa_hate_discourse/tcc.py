3576e83c1eae686fc93a397267121f88d04b8716
J B G <goode@localhost.localdomain>
2023-06-09 14:55:45 -0300

    counting unique users

---
+++ usa_hate_discourse/tcc.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import os
+import gc
+import copy
+import json
+import sys
+from pathlib import Path
+from datetime import datetime, timedelta, date
+import time
+from dateutil.relativedelta import relativedelta 
+
+import pyarrow.parquet as pq
+import pyarrow as pa
+
+from tqdm import tqdm
+
+pd.options.display.max_rows = 100
+pd.options.display.max_columns = 100
+
+pd.options.display.float_format = '{:,.2f}'.format
+
+import warnings
+warnings.filterwarnings("ignore")
+count = 0
+counter = 0
+unique_names = set()
+train_file = "tweets.jsonl"
+with open(train_file, 'r') as file:
+    for line in file:
+        data = json.loads(line)
+
+        name = data['tweet']['user']['username']
+        
+        #if count == 3:
+        #    break
+        if name is not None and name not in unique_names:
+            unique_names.add(name)
+            counter += 1
+        count = count + 1
+
+print(counter)
+print(unique_names)
+with open('output.txt', 'w') as file:
+    file.write(counter)
