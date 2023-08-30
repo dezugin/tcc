9a862a74286655c77a6dd9e447a2e97989150762
zezin <deSouza@protonmail.com>
2023-08-29 21:23:03 -0300

    message size 2016

--- usa_hate_discourse/counter_message_size_2016.py
+++ usa_hate_discourse/counter_message_size_2016.py
@@ -7,20 +7,39 @@ counterTrump = 0
 train_file = "/home/zezin/Documents/tcc/elusa/tweets.jsonl"
 with open(train_file, 'r') as file:
     for line in file:
+        #if count <= stahp:
+         #   count = count + 1
+         #   continue
         data = json.loads(line)
-
-        trump = data['tweet']['semantics']['targeted']['trump']
-        hillary = data['tweet']['semantics']['targeted']['hillary']
+        text = data['tweet']['text']
+        chars = len(text)
+        words = len(text.split())
+        total_chars += chars
+        total_words += words
+        total_tweets += 1
+        with open('2016_tweets.txt', 'a') as file:
+            file.write(str(total_tweets)+"\n")   
+        with open('2016_words.txt', 'a') as file:
+            file.write(str(total_words)+"\n")
+        with open('2016_chars.txt', 'a') as file:
+            file.write(str(total_chars)+"\n")
         #if count == 3:
-        #    break
-        if trump is not None and trump == True:
-            counterTrump += 1
-        if hillary is not None and hillary == True:
-            counterHillary += 1
+         #   break
+        
+        
         count = count + 1
-        print("linha ",count,", ",count/4334627,"%")
-
-with open('trump_2016.txt', 'w') as file:
-    file.write(str(counterTrump))
-with open('hillary_2016.txt', 'w') as file:
-    file.write(str(counterHillary))
+        print(count)
+        
+avg_chars = total_chars / total_tweets if total_tweets > 0 else 0
+avg_words = total_words / total_tweets if total_tweets > 0 else 0
+print(f"Total number of tweets: {total_tweets}")
+print(f"Total character count: {total_chars}")
+print(f"Average characters per tweet: {avg_chars:.2f}")
+print(f"Total word count: {total_words}")
+print(f"Average words per tweet: {avg_words:.2f}")
+with open('message_size_2016.txt', 'w') as file:
+    file.write(f"Total number of tweets: {total_tweets}"+"\n")
+    file.write(f"Total character count: {total_chars}"+"\n")
+    file.write(f"Average characters per tweet: {avg_chars:.2f}"+"\n")
+    file.write(f"Total word count: {total_words}"+"\n")
+    file.write(f"Average words per tweet: {avg_words:.2f}"+"\n")
