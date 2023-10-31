5ba989f5b4f6660ff2d2172c8e6be042d7952ac2
zezin <deSouza@protonmail.com>
2023-10-30 23:36:35 -0300

    redid the dates, one roll per day

--- elusa/topic_bar.py
+++ elusa/topic_bar.py
@@ -6,7 +6,8 @@ from sentence_transformers import SentenceTransformer
 from datetime import datetime
 import os
 from sklearn.feature_extraction.text import CountVectorizer
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 vectorizer_model = CountVectorizer(stop_words="english")
 sentence_model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
@@ -18,7 +19,13 @@ topic_model = BERTopic(embedding_model=sentence_model,
                         vectorizer_model=vectorizer_model)
 
 # Stream data and process it incrementally
-file_path = '/home/zezin/Documents/tcc/elusa/week.jsonl'
+file_path = "/home/zezin/Documents/tcc/elusa/week1d1.jsonl"
+file_path2 = "/home/zezin/Documents/tcc/elusa/week1d2.jsonl"
+file_path3 = "/home/zezin/Documents/tcc/elusa/week1d3.jsonl"
+file_path4 = "/home/zezin/Documents/tcc/elusa/week1d4.jsonl"
+file_path5 = "/home/zezin/Documents/tcc/elusa/week1d5.jsonl"
+file_path6 = "/home/zezin/Documents/tcc/elusa/week1d6.jsonl"
+file_path7 = "/home/zezin/Documents/tcc/elusa/week1d7.jsonl"
 output_file = "/home/zezin/Documents/tcc/elusa/week.csv"
 model_file = '/home/zezin/Documents/tcc/elusa/week_model'
 #topic_model = BERTopic.load(model_file)
@@ -26,14 +33,7 @@ batch_size = 522
 documents = []
 dates = []
 count = 0
-pathexists = False
-if os.path.exists(output_file):
-    pathexists = True
 len_file = 0
-final_counter = 1758509 - 524
-#with open(file_path, 'r') as file:
-#    len_file = num_lines = sum(1 for _ in file)
-#    final_counter = len_file - 524
 with open(file_path, 'r') as f, open('/home/zezin/Documents/tcc/elusa/counter.txt','a') as countfile:
     batch_counter = 0
     for line in f:
@@ -41,11 +41,12 @@ with open(file_path, 'r') as f, open('/home/zezin/Documents/tcc/elusa/counter.tx
         #    count += 1
         #    print (count)
         #    continue
-        print (count)
+        print ('a'+count)
         countfile.write((str(count))+'\n')
         tweet = json.loads(line)
         documents.append(tweet['tweet']['text'])
         tweet_date = datetime.utcfromtimestamp(tweet['tweet']['created_at']['$date'] / 1000).strftime('%Y-%m-%d')
+        print(tweet_date)
         dates.append(tweet_date)
         batch_counter += 1
         count = count + 1
@@ -57,15 +58,226 @@ with open(file_path, 'r') as f, open('/home/zezin/Documents/tcc/elusa/counter.tx
             df = pd.DataFrame.from_dict(a, orient='index')
             df = df.transpose()
             counted_df = df.groupby(['date', 'topic_n']).size().reset_index(name='counts')
-            if pathexists:
-                existing_df = pd.read_csv(output_file)
-                merged_df = pd.merge(existing_df, counted_df, on=['date', 'topic_n'], how='outer').fillna(0)
-                merged_df['counts'] = merged_df['counts_x'] + merged_df['counts_y']
-                merged_df = merged_df.drop(columns=['counts_x', 'counts_y'])
-    
-            else:
-                merged_df = counted_df
-                pathexists = True
+            existing_df = pd.read_csv(output_file)
+            merged_df = pd.merge(existing_df, counted_df, on=['date', 'topic_n'], how='outer').fillna(0)
+            merged_df['counts'] = merged_df['counts_x'] + merged_df['counts_y']
+            merged_df = merged_df.drop(columns=['counts_x', 'counts_y'])
+            topic_model.save(model_file)
+            merged_df.to_csv(output_file, index=False)
+with open(file_path1, 'r') as f, open('/home/zezin/Documents/tcc/elusa/counter.txt','a') as countfile:
+    batch_counter = 0
+    for line in f:
+        #if count <= final_counter: #1565663#1456565: #10000:#200003:#522: #
+        #    count += 1
+        #    print (count)
+        #    continue
+        print ('b'+count)
+        countfile.write((str(count))+'\n')
+        tweet = json.loads(line)
+        documents.append(tweet['tweet']['text'])
+        tweet_date = datetime.utcfromtimestamp(tweet['tweet']['created_at']['$date'] / 1000).strftime('%Y-%m-%d')
+        print(tweet_date)
+        dates.append(tweet_date)
+        batch_counter += 1
+        count = count + 1
+        if batch_counter >= batch_size:
+            topics, _ = topic_model.fit_transform(documents)
+            documents = []  # Clear the batch
+            batch_counter = 0
+            a = {'date': dates, 'topic_n': topics}
+            df = pd.DataFrame.from_dict(a, orient='index')
+            df = df.transpose()
+            counted_df = df.groupby(['date', 'topic_n']).size().reset_index(name='counts')
+            existing_df = pd.read_csv(output_file)
+            merged_df = pd.merge(existing_df, counted_df, on=['date', 'topic_n'], how='outer').fillna(0)
+            merged_df['counts'] = merged_df['counts_x'] + merged_df['counts_y']
+            merged_df = merged_df.drop(columns=['counts_x', 'counts_y'])
+            topic_model.save(model_file)
+            merged_df.to_csv(output_file, index=False)
+
+with open(file_path2, 'r') as f, open('/home/zezin/Documents/tcc/elusa/counter.txt','a') as countfile:
+    batch_counter = 0
+    for line in f:
+        #if count <= final_counter: #1565663#1456565: #10000:#200003:#522: #
+        #    count += 1
+        #    print (count)
+        #    continue
+        print ('c'+count)
+        countfile.write((str(count))+'\n')
+        tweet = json.loads(line)
+        documents.append(tweet['tweet']['text'])
+        tweet_date = datetime.utcfromtimestamp(tweet['tweet']['created_at']['$date'] / 1000).strftime('%Y-%m-%d')
+        print(tweet_date)
+        dates.append(tweet_date)
+        batch_counter += 1
+        count = count + 1
+        if batch_counter >= batch_size:
+            topics, _ = topic_model.fit_transform(documents)
+            documents = []  # Clear the batch
+            batch_counter = 0
+            a = {'date': dates, 'topic_n': topics}
+            df = pd.DataFrame.from_dict(a, orient='index')
+            df = df.transpose()
+            counted_df = df.groupby(['date', 'topic_n']).size().reset_index(name='counts')
+            existing_df = pd.read_csv(output_file)
+            merged_df = pd.merge(existing_df, counted_df, on=['date', 'topic_n'], how='outer').fillna(0)
+            merged_df['counts'] = merged_df['counts_x'] + merged_df['counts_y']
+            merged_df = merged_df.drop(columns=['counts_x', 'counts_y'])
+            topic_model.save(model_file)
+            merged_df.to_csv(output_file, index=False)
+
+with open(file_path3, 'r') as f, open('/home/zezin/Documents/tcc/elusa/counter.txt','a') as countfile:
+    batch_counter = 0
+    for line in f:
+        #if count <= final_counter: #1565663#1456565: #10000:#200003:#522: #
+        #    count += 1
+        #    print (count)
+        #    continue
+        print ('d'+count)
+        countfile.write((str(count))+'\n')
+        tweet = json.loads(line)
+        documents.append(tweet['tweet']['text'])
+        tweet_date = datetime.utcfromtimestamp(tweet['tweet']['created_at']['$date'] / 1000).strftime('%Y-%m-%d')
+        print(tweet_date)
+        dates.append(tweet_date)
+        batch_counter += 1
+        count = count + 1
+        if batch_counter >= batch_size:
+            topics, _ = topic_model.fit_transform(documents)
+            documents = []  # Clear the batch
+            batch_counter = 0
+            a = {'date': dates, 'topic_n': topics}
+            df = pd.DataFrame.from_dict(a, orient='index')
+            df = df.transpose()
+            counted_df = df.groupby(['date', 'topic_n']).size().reset_index(name='counts')
+            existing_df = pd.read_csv(output_file)
+            merged_df = pd.merge(existing_df, counted_df, on=['date', 'topic_n'], how='outer').fillna(0)
+            merged_df['counts'] = merged_df['counts_x'] + merged_df['counts_y']
+            merged_df = merged_df.drop(columns=['counts_x', 'counts_y'])
+            topic_model.save(model_file)
+            merged_df.to_csv(output_file, index=False)
+
+with open(file_path4, 'r') as f, open('/home/zezin/Documents/tcc/elusa/counter.txt','a') as countfile:
+    batch_counter = 0
+    for line in f:
+        #if count <= final_counter: #1565663#1456565: #10000:#200003:#522: #
+        #    count += 1
+        #    print (count)
+        #    continue
+        print ('e'+count)
+        countfile.write((str(count))+'\n')
+        tweet = json.loads(line)
+        documents.append(tweet['tweet']['text'])
+        tweet_date = datetime.utcfromtimestamp(tweet['tweet']['created_at']['$date'] / 1000).strftime('%Y-%m-%d')
+        print(tweet_date)
+        dates.append(tweet_date)
+        batch_counter += 1
+        count = count + 1
+        if batch_counter >= batch_size:
+            topics, _ = topic_model.fit_transform(documents)
+            documents = []  # Clear the batch
+            batch_counter = 0
+            a = {'date': dates, 'topic_n': topics}
+            df = pd.DataFrame.from_dict(a, orient='index')
+            df = df.transpose()
+            counted_df = df.groupby(['date', 'topic_n']).size().reset_index(name='counts')
+            existing_df = pd.read_csv(output_file)
+            merged_df = pd.merge(existing_df, counted_df, on=['date', 'topic_n'], how='outer').fillna(0)
+            merged_df['counts'] = merged_df['counts_x'] + merged_df['counts_y']
+            merged_df = merged_df.drop(columns=['counts_x', 'counts_y'])
+            topic_model.save(model_file)
+            merged_df.to_csv(output_file, index=False)
+
+with open(file_path5, 'r') as f, open('/home/zezin/Documents/tcc/elusa/counter.txt','a') as countfile:
+    batch_counter = 0
+    for line in f:
+        #if count <= final_counter: #1565663#1456565: #10000:#200003:#522: #
+        #    count += 1
+        #    print (count)
+        #    continue
+        print ('e'+count)
+        countfile.write((str(count))+'\n')
+        tweet = json.loads(line)
+        documents.append(tweet['tweet']['text'])
+        tweet_date = datetime.utcfromtimestamp(tweet['tweet']['created_at']['$date'] / 1000).strftime('%Y-%m-%d')
+        print(tweet_date)
+        dates.append(tweet_date)
+        batch_counter += 1
+        count = count + 1
+        if batch_counter >= batch_size:
+            topics, _ = topic_model.fit_transform(documents)
+            documents = []  # Clear the batch
+            batch_counter = 0
+            a = {'date': dates, 'topic_n': topics}
+            df = pd.DataFrame.from_dict(a, orient='index')
+            df = df.transpose()
+            counted_df = df.groupby(['date', 'topic_n']).size().reset_index(name='counts')
+            existing_df = pd.read_csv(output_file)
+            merged_df = pd.merge(existing_df, counted_df, on=['date', 'topic_n'], how='outer').fillna(0)
+            merged_df['counts'] = merged_df['counts_x'] + merged_df['counts_y']
+            merged_df = merged_df.drop(columns=['counts_x', 'counts_y'])
+            topic_model.save(model_file)
+            merged_df.to_csv(output_file, index=False)
+
+with open(file_path6, 'r') as f, open('/home/zezin/Documents/tcc/elusa/counter.txt','a') as countfile:
+    batch_counter = 0
+    for line in f:
+        #if count <= final_counter: #1565663#1456565: #10000:#200003:#522: #
+        #    count += 1
+        #    print (count)
+        #    continue
+        print ('f'+count)
+        countfile.write((str(count))+'\n')
+        tweet = json.loads(line)
+        documents.append(tweet['tweet']['text'])
+        tweet_date = datetime.utcfromtimestamp(tweet['tweet']['created_at']['$date'] / 1000).strftime('%Y-%m-%d')
+        print(tweet_date)
+        dates.append(tweet_date)
+        batch_counter += 1
+        count = count + 1
+        if batch_counter >= batch_size:
+            topics, _ = topic_model.fit_transform(documents)
+            documents = []  # Clear the batch
+            batch_counter = 0
+            a = {'date': dates, 'topic_n': topics}
+            df = pd.DataFrame.from_dict(a, orient='index')
+            df = df.transpose()
+            counted_df = df.groupby(['date', 'topic_n']).size().reset_index(name='counts')
+            existing_df = pd.read_csv(output_file)
+            merged_df = pd.merge(existing_df, counted_df, on=['date', 'topic_n'], how='outer').fillna(0)
+            merged_df['counts'] = merged_df['counts_x'] + merged_df['counts_y']
+            merged_df = merged_df.drop(columns=['counts_x', 'counts_y'])
+            topic_model.save(model_file)
+            merged_df.to_csv(output_file, index=False)
+
+with open(file_path7, 'r') as f, open('/home/zezin/Documents/tcc/elusa/counter.txt','a') as countfile:
+    batch_counter = 0
+    for line in f:
+        #if count <= final_counter: #1565663#1456565: #10000:#200003:#522: #
+        #    count += 1
+        #    print (count)
+        #    continue
+        print ('g'+count)
+        countfile.write((str(count))+'\n')
+        tweet = json.loads(line)
+        documents.append(tweet['tweet']['text'])
+        tweet_date = datetime.utcfromtimestamp(tweet['tweet']['created_at']['$date'] / 1000).strftime('%Y-%m-%d')
+        print(tweet_date)
+        dates.append(tweet_date)
+        batch_counter += 1
+        count = count + 1
+        if batch_counter >= batch_size:
+            topics, _ = topic_model.fit_transform(documents)
+            documents = []  # Clear the batch
+            batch_counter = 0
+            a = {'date': dates, 'topic_n': topics}
+            df = pd.DataFrame.from_dict(a, orient='index')
+            df = df.transpose()
+            counted_df = df.groupby(['date', 'topic_n']).size().reset_index(name='counts')
+            existing_df = pd.read_csv(output_file)
+            merged_df = pd.merge(existing_df, counted_df, on=['date', 'topic_n'], how='outer').fillna(0)
+            merged_df['counts'] = merged_df['counts_x'] + merged_df['counts_y']
+            merged_df = merged_df.drop(columns=['counts_x', 'counts_y'])
             topic_model.save(model_file)
             merged_df.to_csv(output_file, index=False)
 
@@ -82,3 +294,25 @@ df['topic'] = df['topic_n'].map(topic_name_mapping)
 fig = px.bar(df, x='date', y='counts', color='topic', title="Number of Tweets per Topic over a Week")
 fig.write_html("/home/zezin/Documents/tcc/elusa/week.html")
 fig.show()
+
+
+sums = df.groupby('topic')['counts'].sum().reset_index()
+sums['date'] = 'All Days'
+df = pd.concat([df, sums], ignore_index=True)
+plt.figure(figsize=(15, 10))
+sns.barplot(data=df, x='date', y='counts', hue='topic_n', ci=None)
+
+# Set the title and labels
+plt.title('Number of Tweets per Topic per Day')
+plt.xlabel('Date')
+plt.ylabel('Number of Tweets')
+plt.legend(title='Topics', bbox_to_anchor=(1.05, 1), loc='upper left')
+
+# Rotate the x-axis labels for better readability
+plt.xticks(rotation=45)
+
+# Adjust the layout
+plt.tight_layout()
+
+# Show the plot
+plt.show()
--- usa_hate_discourse/ubozaba.py
+++ usa_hate_discourse/ubozaba.py
@@ -1,33 +1,32 @@
 import json
-from sentistrength import PySentiStr
-import pandas as pd
-from sklearn.cluster import KMeans
-from pyclustering.cluster import xmeans
-from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
-senti = PySentiStr()
-senti.setSentiStrengthPath('/home/zezin/SentiStrength.jar')
-senti.setSentiStrengthLanguageFolderPath('/home/zezin/SentiStrength_Data/')
-
-count = 0
-
-intermediate_file = "/home/zezin/Documents/tcc/elusa/tweets.jsonl"
-
-final_file = "/home/zezin/Documents/tcc/elusa/week1p1.jsonl"
-final_file2 = "/home/zezin/Documents/tcc/elusa/week1p2.jsonl"
-final_file3 = "/home/zezin/Documents/tcc/elusa/week1p3.jsonl"
-
-with open(intermediate_file, 'r') as file, open(final_file, 'a') as outfile, open(final_file2, 'a') as outfile2, open(final_file3, 'a') as outfile3,open('count.txt', 'a') as countfile:
+tweets_file = "/home/zezin/Documents/tcc/elusa/tweets.jsonl"
+final_file = "/home/zezin/Documents/tcc/elusa/week1d1.jsonl"
+final_file2 = "/home/zezin/Documents/tcc/elusa/week1d2.jsonl"
+final_file3 = "/home/zezin/Documents/tcc/elusa/week1d3.jsonl"
+final_file4 = "/home/zezin/Documents/tcc/elusa/week1d4.jsonl"
+final_file5 = "/home/zezin/Documents/tcc/elusa/week1d5.jsonl"
+final_file6 = "/home/zezin/Documents/tcc/elusa/week1d6.jsonl"
+final_file7 = "/home/zezin/Documents/tcc/elusa/week1d7.jsonl"
+with open(tweets_file, 'r') as file, open(final_file, 'a') as outfile, open(final_file2, 'a') as outfile2, open(final_file3, 'a') as outfile3,open('count.txt', 'a') as countfile,open(final_file4, 'a') as outfile4,open(final_file5, 'a') as outfile5,open(final_file6, 'a') as outfile6,open(final_file7, 'a') as outfile7:
     for idx, line in enumerate(file):
         #if idx == 3:
         #    break
         data = json.loads(line)
         timestamp = data['tweet']['created_at']['$date']
         #print(timestamp)
-        if timestamp < 1474858800000 and timestamp > 1474599600000:#week 1#1 23-09/26-09 3am 
+        if timestamp < 1474675200000 and timestamp >= 1474588800000:#day 1#1 23-09/26-09 12am 
             outfile.write(json.dumps(data) + '\n')
-        if timestamp < 1474945200000 and timestamp > 1474858800000:#week 1#2 26-09/27-09 3am 
+        if timestamp < 1474761600000 and timestamp >= 1474675200000:#day 2#2 24-09 12am 
             outfile2.write(json.dumps(data) + '\n')
-        if timestamp < 1475204400000 and timestamp > 1474945200000:#week 1#3 27-09/30-09 3am 
+        if timestamp < 1474848000000 and timestamp >= 1474761600000:#day 3#3 25-09 12am 
             outfile3.write(json.dumps(data) + '\n')
+        if timestamp < 1474934400000 and timestamp >= 1474848000000:#day 4#1 26-09 12am 
+            outfile4.write(json.dumps(data) + '\n')
+        if timestamp < 1475020800000 and timestamp >= 1474934400000:#day 5#2 27-09 12am 
+            outfile5.write(json.dumps(data) + '\n')
+        if timestamp < 1475107200000 and timestamp >= 1475020800000:#day 6#3 28-09 12am 
+            outfile6.write(json.dumps(data) + '\n')
+        if timestamp < 1475193600000 and timestamp >= 1475107200000:#day 7#3 29-09 12am 
+            outfile7.write(json.dumps(data) + '\n')
         countfile.write(str(idx)+"\n")  
         print(idx)
