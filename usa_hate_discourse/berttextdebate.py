e46887c64e4ed439ba814b9438550e6b7f1a3802
zezin <deSouza@protonmail.com>
2023-11-05 19:16:47 -0300

    ran 3 keybert

--- usa_hate_discourse/berttextdebate.py
+++ usa_hate_discourse/berttextdebate.py
@@ -11,9 +11,9 @@ keywords_counter_trump = defaultdict(int)
 keywords_counter_clinton = defaultdict(int)
 
 # Path to your jsonl file
-file_path = "/home/zezin/Documents/tcc/usa_hate_discourse/week1debate.txt"
-trump_path = "/home/zezin/Documents/tcc/usa_hate_discourse/week1debatetrump.txt"
-clinton_path = "/home/zezin/Documents/tcc/usa_hate_discourse/week1debateclinton.txt"
+file_path = "/home/zezin/Documents/tcc/usa_hate_discourse/week3debate.txt"
+trump_path = "/home/zezin/Documents/tcc/usa_hate_discourse/week3debatetrump.txt"
+clinton_path = "/home/zezin/Documents/tcc/usa_hate_discourse/week3debateclinton.txt"
 count = 0
 # Loop through the JSONL file and extract keywords for each tweet's text
 with open(file_path, 'r') as file, open(trump_path, 'a') as trumpfile, open(clinton_path, 'a') as clintonfile:
@@ -22,18 +22,15 @@ with open(file_path, 'r') as file, open(trump_path, 'a') as trumpfile, open(clin
         #if count > 2:
         #    break
         text = line
-
-        if "HOLT:" in text:
-            continue
-        if "TRUMP:" in text:
+        if "Trump:" in text:
             #trumpfile.write(line + '\n')
-            text = text.replace('TRUMP:', '')
+            text = text.replace('Trump:', '')
             keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 1), top_n=5)
             for keyword, _ in keywords:
                 keywords_counter_trump[keyword] += 1
-        if "CLINTON:" in text:    
+        if "Clinton:" in text:    
             #clintonfile.write(line + '\n')
-            text = text.replace('CLINTON:', '')
+            text = text.replace('Clinton:', '')
             keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 1), top_n=5)
             for keyword, _ in keywords:
                 keywords_counter_clinton[keyword] += 1
@@ -57,9 +54,9 @@ for keyword, count in top_keywords_trump:
     print(keyword, count)
 for keyword, count in top_keywords_clinton:
     print(keyword, count)
-week1_result_path = "/home/zezin/Documents/tcc/elusa/week1debate_result.txt"
-week1_result_path_trump = "/home/zezin/Documents/tcc/elusa/week1debate_result_trump.txt"
-week1_result_path_clinton = "/home/zezin/Documents/tcc/elusa/week1debate_result_clinton.txt"
+week1_result_path = "/home/zezin/Documents/tcc/elusa/week3debate_result.txt"
+week1_result_path_trump = "/home/zezin/Documents/tcc/elusa/week3debate_result_trump.txt"
+week1_result_path_clinton = "/home/zezin/Documents/tcc/elusa/week3debate_result_clinton.txt"
 with open(week1_result_path, 'a') as file,open(week1_result_path_trump, 'a') as trumpfile,open(week1_result_path_clinton, 'a') as clintonfile:
     file.write("Top 100 keywords and their counts:\n")
     trumpfile.write("Top 100 keywords and their counts:\n")
